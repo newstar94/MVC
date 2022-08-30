e027393d6c55d9767367b52c32602127b5714a93
newstar94 <newstar94@gmail.com>
2022-08-30 09:13:31 +0700

    S3.P1
    S3.P1

---
+++ S3_P1_T2104E_DuongVT/S3_P1_T2104E_DuongVT/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S3_P1_T2104E_DuongVT.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["Name"] = "Nguyễn Văn A";
+            TempData["Age"] = "18";
+            TempData["Address"] = "Hà Nội";
+            return RedirectToAction("ShowData");
+        }
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+    }
+}
---
+++ S3_P1_T2104E_DuongVT/S3_P1_T2104E_DuongVT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S3_P1_T2104E_DuongVT.MvcApplication" Language="C#" %>
