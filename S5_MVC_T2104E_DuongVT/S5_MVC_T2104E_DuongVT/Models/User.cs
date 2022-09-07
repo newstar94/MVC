63363fc532c17688a68dfc7a58ca6287675c55d2
newstar94 <newstar94@gmail.com>
2022-09-07 16:49:19 +0700

    S5

---
+++ S5_MVC_T2104E_DuongVT/S5_MVC_T2104E_DuongVT/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace S5_MVC_T2104E_DuongVT.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        public string Re_Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
