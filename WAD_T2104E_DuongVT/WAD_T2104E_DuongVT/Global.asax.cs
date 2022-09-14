dc1df731346bb380b63ce5732b7382efc7129c5a
newstar94 <newstar94@gmail.com>
2022-09-14 10:13:11 +0700

    update

--- WAD_T2104E_DuongVT/WAD_T2104E_DuongVT/Global.asax.cs
+++ WAD_T2104E_DuongVT/WAD_T2104E_DuongVT/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WAD_T2104E_DuongVT.Models;
 
 namespace WAD_T2104E_DuongVT
 {
@@ -16,6 +18,7 @@ namespace WAD_T2104E_DuongVT
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Data>());
         }
     }
 }
